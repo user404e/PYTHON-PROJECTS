2d0d149655b83e7784b0fcde755032e21ce2ac97
user404e <e404user@gmail.com>
2023-05-31 09:02:14 +0000

    PYTHON

--- DOUBLE DICE ROLLING SIMULATOR/DoubleDiceRolling.py
+++ DOUBLE DICE ROLLING SIMULATOR/DoubleDiceRolling.py
@@ -11,7 +11,8 @@ while(choice != 0):
     choice = int(input("\n_ : "))
     
     if(choice == 1):
-        print("\nDice =",randint(1,6))
+        print("\nDice 1 =",randint(1,6))
+        print("Dice 2 =",randint(1,6))
     elif(choice == 0):
         print("\nExited -")
     else:
